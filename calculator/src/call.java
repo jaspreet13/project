1f0d9635f3e508aa18927970d18cbfdec2e4b07e
jaspreet <jaspreet@gmail.com>
2019-05-20 20:37:41 -0700

    adding

---
+++ calculator/src/call.java
@@ -0,0 +1,10 @@
+import static org.junit.assert.*;
+import org.junit.test;
+public class sample test{
+    @test
+    public void sample test(){
+        int a=5;
+        int b=5;
+        assert true(a+b==3);
+    }
+}
